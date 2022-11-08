0af5a11f6d0b16c569c39d6ac61cb4ca00a49b5a
Hi-nu <108650180+Hi-nu@users.noreply.github.com>
2022-11-08 14:58:11 +0900

    Update index.js

--- API/database/index.js
+++ API/database/index.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 
 const config = {
-  host: process.env.CORONOW_MYSQL_HOST || '127.0.0.1',
+  host: process.env.CORONOW_MYSQL_HOST || 'coronow.c0g7u8xpkve4.ap-northeast-2.rds.amazonaws.com',
   port: 3306,
   database: 'coronow',
   user: 'coronow_admin',
-  password: process.env.CORONOW_MYSQL_PASSWORD || 'rabbit1104',
+  password: process.env.CORONOW_MYSQL_PASSWORD || 'rabbit110#',
 };
 
 const sequelize = new Sequelize(config.database, config.user, config.password, {
